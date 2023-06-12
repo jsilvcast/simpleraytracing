4fe749bcc674ad9d365b5ae5f7eb67f651addb70
v <josesilvacastro1994@gmail.com>
2023-06-12 22:42:04 +0200

    Updated code with zenith and front view implementations

--- raytracing.py
+++ raytracing.py
@@ -25,8 +25,8 @@ SOFTWARE.
 import numpy as np
 import matplotlib.pyplot as plt
 
-w = 400
-h = 300
+w = 800
+h = 600
 
 
 def normalize(x):
@@ -260,21 +260,33 @@ specular_k = 50
 
 depth_max = 5  # Maximum number of light reflections.
 col = np.zeros(3)  # Current color.
-O = np.array([0., 0.35, -1.])  # Camera.
-Q = np.array([0., 0., 0.])  # Camera pointing to.
-img = np.zeros((h, w, 3))
 
+camera = "zenithal" #can be "cenital" or "front"
+
+img = np.zeros((h, w, 3))
 r = float(w) / h
-# Screen coordinates: x0, y0, x1, y1.
-S = (-1., -1. / r + .25, 1., 1. / r + .25)
+if camera == "front":
+    O = np.array([0., 0.35, -1.])  # Camera.
+    Q = np.array([0., 0., 0.])  # Camera pointing to.
+    S = (-1., -1. / r + .25, 1., 1. / r + .25)
+
+elif camera == "zenith":
+    O = np.array([0., 9, 0.])  # Camera.
+    Q = np.array([0., 1, 0.])  # Camera pointing to.
+    S = (-4., 0. / r + .25, 2., 6. / r + .25)
+
 
 # Loop through all pixels.
 for i, x in enumerate(np.linspace(S[0], S[2], w)):
     if i % 10 == 0:
         print(i / float(w) * 100, "%")
-    for j, y in enumerate(np.linspace(S[1], S[3], h)):
+    for j, yz in enumerate(np.linspace(S[1], S[3], h)):
         col[:] = 0
-        Q[:2] = (x, y)
+        if camera == "front":
+            Q[:2] = (x, yz) # yz is y
+        elif camera == "zenith":
+            Q[0] = x
+            Q[2] = yz # yz is z
         D = normalize(Q - O)
         depth = 0
         rayO, rayD = O, D
